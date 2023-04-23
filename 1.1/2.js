92f5b1545aa88209930f88d6855f98bf2b132b28
saikomatyny <120598341+saikomatyny@users.noreply.github.com>
2023-04-24 00:56:50 +0300

    Update 2.js

--- 1.1/2.js
+++ 1.1/2.js
@@ -1,26 +1,29 @@
 
 let validateEmail = function(email){
-    const check = 1;
-    return true;
+    const check = /(^[a-zA-Z]|\d)((\-|\.|\+)|[a-zA-Z]|\d){1,19}@(((\.|\!|\$|\%|\&|\’|\*|\+|\/|\=|\?|\^|\_|\-)|\w|\d){1,15})\.([a-zA-Z]{1,5}$)/gm;
+    return check.test(email);
 };
 
 let validatePhone = function(phone){
-    const check = 1;
-    return false;
+    if (phone.length > 25) {
+        return false; 
+    };
+    const check = /^(\+38|\-+|\s+?)?\s*?\(?\-*?\d\-*?\d\-*?\d\)?\s?(\d(\-|\s)*?){7}$/;
+    return check.test(phone);
 };
 
 let validatePassword = function(password){
-    const check = /[]/;
-    return '123';
+    const check = /(?=.*[A-Z])(?=.*[a-z])(?=.*\d).{8,}/gm;
+    return check.test(password);
 };
 
 
 let Validator = new Map();
 
 
-Validator.set(validateEmail, validateEmail('aaa@a.end'));
-Validator.set(validatePhone, validatePhone('+380967245128'));
-Validator.set(validatePassword, validatePassword('password123_'));
+Validator.set(validateEmail, validateEmail('aaa@aa.end'));
+Validator.set(validatePhone, validatePhone('+38 0967245128'));
+Validator.set(validatePassword, validatePassword('passworD123_'));
 
 
 console.log(Validator.get(validateEmail));
